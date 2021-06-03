e6e893dec9c7046c793f3e076d1182abc4d63443
Nirmala Luwang <nirmalaluwang@gmail.com>
2021-06-03 20:18:34 +0530

    bookings index updates

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :bikes, only: [ :index, :show, :new, :create, :destroy] do
-    resources :bookings, only: [ :new, :create ]
+    resources :bookings, only: [ :new, :create, :index ]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
@@ -14,4 +14,3 @@ end
 # post bikes to: bikes#create
 # get bikes/:id/booking/new to bookings#new
 # post bikes/:id/bookings to bookings#create
-

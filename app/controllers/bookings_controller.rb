f7844e29cad0d89ace9447595488be062c1c63dd
Nirmala Luwang <nirmalaluwang@gmail.com>
2021-06-03 19:16:36 +0530

    index map, booking controller/form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,26 +1,28 @@
 class BookingsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
     @bookings = Booking.all
+    authorize(@booking)
   end
 
-  def new
-    @booking = Booking.new
-    @bike = Bike.find(params[:bike_id])
-  end
   
   def create
     @booking = Booking.new(booking_params)
     @bike = Bike.find(params[:bike_id])
-    @booking.bike = @booking
-      if @bookmark.save
-        redirect_to bike_path(@list)
+    @booking.bike = @bike
+    @booking.user = current_user
+    authorize(@booking)
+
+      if @booking.save
+        redirect_to bike_path(@bike), notice: 'Booking was made'
       else
-        render :show
+        render "bikes/show"
       end
   end
 
   private
-    def bookmark_params
-      params.require(:bookmark).permit(:comment, :movie_id)
+    def booking_params
+      params.require(:booking).permit(:accepted, :dates)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -45,7 +45,6 @@ ActiveRecord::Schema.define(version: 2021_06_03_092044) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "description"
-    t.string "price"
     t.float "latitude"
     t.float "longitude"
     t.index ["user_id"], name: "index_bikes_on_user_id"

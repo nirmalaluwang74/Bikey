8c3c94e34c235852fcc2169366eb56d0ad96dd12
Nirmala Luwang <nirmalaluwang@gmail.com>
2021-06-04 17:21:35 +0530

    bookings index

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -1,9 +1,11 @@
 class BookingPolicy < ApplicationPolicy
+
   class Scope < Scope
     def resolve
-      scope.all
+      scope.joins(:bike).where(bikes: {user: user} )
     end
   end
+
   def create?
     true
   end
